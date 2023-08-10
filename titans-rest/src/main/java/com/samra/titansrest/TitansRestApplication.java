064a1fb5e367b05365090d04c64dd72b4dfb2b7f
nightwing-pav <pavindersingh1996@icloud.com>
2023-08-10 16:43:24 +0100

    creating new rest backend for ui

---
+++ titans-rest/src/main/java/com/samra/titansrest/TitansRestApplication.java
@@ -0,0 +1,13 @@
+package com.samra.titansrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitansRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitansRestApplication.class, args);
+	}
+
+}
---
+++ titans-rest/src/test/java/com/samra/titansrest/TitansRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samra.titansrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitansRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
